de44144c6b161841ca4df38c72e87aed866403f3
Oliver Jaksch <ojaksch@com-in.de>
2022-08-01 11:17:26 +0200

    Installer: force - MCUtype

--- installer.sh
+++ installer.sh
@@ -121,6 +121,7 @@ if [ "${1}" = "FORCE" ]; then
     echo -e "${fyellow}Version of your tty2tft device is ${fblue}${SWver}${fyellow}, forced BUILDVER is ${fgreen}${BUILDVER}${fyellow}.${freset}"
     echo -e "${fyellow}MCUtype is set to ${fblue}${MCUtype}${freset}"
     if ! [ -z "${2}" ]; then
+      MCUtype="TFTESP32"
       TTY2TFT_DISPLAY="${2}"
       flashesp stage2 "${TTY2TFT_DISPLAY}"
     else
